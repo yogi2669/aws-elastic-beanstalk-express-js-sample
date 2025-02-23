543ef36fd3364c693ac8ae50f60e389e2aa04d1d
yogi2669 <yogeshsrivatsava2003@gmail.com>
2025-02-23 23:28:46 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Yogesh,welcome to Cprime.This is updated version.'));
+app.get('/', (req, res) => res.send('Hello Yogesh,welcome to Cprime.This is updated version.you should finish ur project by tomorrow.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
